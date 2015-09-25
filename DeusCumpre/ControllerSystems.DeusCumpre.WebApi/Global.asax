63b37b18ca9044c59e41979ee5f5e815d358d351
controllersystems <rodrigo.cruz@outlook.com>
2015-09-25 03:17:39 -0300

    Efetuar login

---
+++ DeusCumpre/ControllerSystems.DeusCumpre.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ControllerSystems.DeusCumpre.WebApi.WebApiApplication" Language="C#" %>
---
+++ DeusCumpre/DeusCumpre.Tests.UnitTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControllerSystems.DeusCumpre.Domain.Entities;
+using ControllerSystems.DeusCumpre.Application.Services;
+using ControllerSystems.DeusCumpre.Data.Repositories;
+
+namespace DeusCumpre.Tests.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LogIn()
+        {
+            User user = new User()
+            {
+                Login = "rodrigo.cruz",
+                Password = "1234"
+            };
+            bool result = user.LogIn(new Authentication(new UserRepository()));
+
+            Assert.AreEqual(false, result);
+        }
+    }
+}
